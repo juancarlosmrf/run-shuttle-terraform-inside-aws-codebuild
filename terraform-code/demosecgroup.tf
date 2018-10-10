6b2ccc125a33486a795e652b2ae0457e57c2898c
juancarlosmrf <juancarlos.perez@marfeel.com>
2018-10-10 18:31:19 +0200

    Fix: Unsupported IP protocol TCP ---> tcp

--- terraform-code/demosecgroup.tf
+++ terraform-code/demosecgroup.tf
@@ -7,13 +7,13 @@ resource "aws_security_group" "demosg" {
   egress {
     from_port   = 0
     to_port     = 65535 # All outbound traffic
-    protocol    = "TCP"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
    }
    ingress {
      from_port   = 443
      to_port     = 443 # All outbound traffic
-     protocol    = "TCP"
+     protocol    = "tcp"
      cidr_blocks = ["0.0.0.0/0"]
     }
 }
